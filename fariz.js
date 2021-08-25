37f86a2d1ef078c61b5a5643b98cf944d50d1bf9
setiawannuha <setiawannuha123@gmail.com>
2021-08-25 15:59:33 +0700

    revert & resolve

---
+++ fariz.js
@@ -0,0 +1,2 @@
+// Branch Fariz
+console.log("Fariz")
--- rouf.js
+++ rouf.js
@@ -1,11 +1,1 @@
 console.log("test");
-
-console.log("penambahan fitur");
-
-console.log("Perbaikan fitur")
-
-const getData = () => {
-  return ["satu", "dua", "tiga"]
-}
-const data = ['hallo']
-
